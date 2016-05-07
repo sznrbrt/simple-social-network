e7db35a52b708008707f7d4fc6dd93c5dde78b2f
Norbert Szucs <szucs.norbert@outlook.com>
2016-05-07 10:03:29 -0700

    added check to secret for debugging

--- models/user.js
+++ models/user.js
@@ -5,6 +5,10 @@ var jwt = require('jsonwebtoken');
 
 const JWT_SECRET = process.env.JWT_SECRET;
 
+if(!JWT_SECRET) {
+  throw new Error('Missing JWT_SECRET');
+}
+
 var userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
