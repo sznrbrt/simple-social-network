2973454ad7bce2bf2f1749fa463772e618ed5a0b
Norbert Szucs <szucs.norbert@outlook.com>
2016-05-06 23:15:13 -0700

    heroku config set

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@
 var mongoose = require('mongoose');
 var jwt = require('jsonwebtoken');
 
-const JWT_SECRET = "saddad3434sjahdjd343242sadjah343242";
+const JWT_SECRET = process.env.JWT_SECRET;
 
 var userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
